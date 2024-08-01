548fc36ee70b44debc4885a98a163c38577ae19e
Salem <spencernavarro2004@gmail.com>
2024-07-31 17:38:37 -0700

    feat(lib): Added Clan enum

---
+++ src/lib/enums/Clan.ts
@@ -0,0 +1,69 @@
+/**
+ * Clans a user can join
+ */
+export enum Clan {
+	/**
+	 * The clan of Ameno
+	 */
+	Ameno = "Ameno",
+	/**
+	 * The clan of Angel
+	 */
+	Angel = "Angel",
+	/**
+	 * The clan of Cryo
+	 */
+	Cryo = "Cryo",
+	/**
+	 * The clan of Crystal
+	 */
+	Crystal = "Crystal",
+	/**
+	 * The clan of Darkness
+	 */
+	Darkness = "Darkness",
+	/**
+	 * The clan of Demon
+	 */
+	Demon = "Demon",
+	/**
+	 * The clan of Dendro
+	 */
+	Dendro = "Dendro",
+	/**
+	 * The clan of Electro
+	 */
+	Electro = "Electro",
+	/**
+	 * The clan of Geo
+	 */
+	Geo = "Geo",
+	/**
+	 * The clan of Ghost
+	 */
+	Ghost = "Ghost",
+	/**
+	 * The clan of Hydro
+	 */
+	Hydro = "Hydro",
+	/**
+	 * The clan of Jester
+	 */
+	Jester = "Jester",
+	/**
+	 * The clan of Light
+	 */
+	Light = "Light",
+	/**
+	 * A clan has not been chosen
+	 */
+	None = "None",
+	/**
+	 * The clan of Pyro
+	 */
+	Pyro = "Pyro",
+	/**
+	 * The clan of Witches
+	 */
+	Witch = "Witch"
+}
--- src/lib/enums/index.ts
+++ src/lib/enums/index.ts
@@ -1,2 +1,3 @@
 export * from "#lib/enums/AuthorizationLevel";
 export * from "#lib/enums/Color";
+export * from "#lib/enums/Clan";
