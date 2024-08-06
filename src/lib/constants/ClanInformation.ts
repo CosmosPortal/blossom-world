ea4298b5c3e2397d101172917e5c2e1db8b4b447
Salem <spencernavarro2004@gmail.com>
2024-08-05 23:02:56 -0700

    chore(lib): Added @lib/constants / Clan information

---
+++ src/lib/constants/ClanInformation.ts
@@ -0,0 +1,125 @@
+/**
+ * An array of data containing, names, description and more related to each clan
+ */
+export const ClanInformation = [
+	{
+		id: "1",
+		name: "Anemo",
+		primaryName: "Clan of Anemos",
+		leader: "Zephyr",
+		description: "The Clan of Anemos' main focus is keeping the weather safe for our region. Using the Staff of Winds, they can control the weather with the strong wind the staff can cast. If ever needed, its wind can cut through mountains and start a supercell thunderstorm in seconds. Clan members stick to themselves to avoid drama between the clans.",
+		onboarding: "Oh, hey there! I'm Zephyr, leader of Anemo! You must be {user.tag}, our new member. Here's some information about our clan!\n\nWe, the Clan of Anemo, are mainly focused on keeping the weather safe for our region. Using our Staff of Winds, we can control the weather with the strong wind the staff can cast. If we ever needed, its winds can cut through mountains and start a supercell thunderstorm in seconds. Most of the time, we stick to ourselves to avoid drama between the clans."
+	},
+	{
+		id: "2",
+		name: "Angel",
+		primaryName: "Clan of Angels",
+		leader: "Evangeline",
+		description: "They are a friendly and intelligent clan of angels. Clan members are bright and quick in every aspect. Before you have the chance to blink, an angel will have you frozen from the inside using the Ice Blade. They are supportive of their own and will help each other when they're at their worst.",
+		onboarding: "Oh, hello there! You must be the new angel I was told about. – I am Evangeline, leader of the Angel Clan.\n\nWe are a friendly and intelligent clan of angels. Our clan members are bright and quick in every aspect. Before you have the chance to blink, an angel will have you frozen from the inside using the Ice Blade. We are supportive of our angels and will help each other when we're at our worst."
+	},
+	{
+		id: "3",
+		name: "Cryo",
+		primaryName: "Clan of Cryos",
+		leader: "Chione",
+		description: "The Clan of Cryos can be found in the mountains where they work on improving their powers. They are responsible for creating the Ice Blade used by the Clan of Angels. The only weapon this clan needs is the Ring of Ice. Using the ring, they can make anything ice-related.",
+		onboarding: "Hi there! I'm clan leader, Chione! Welcome to our clan, {user.tag}. Shall we get started with your onboarding?\n\nOur clan is located in the mountains where we focus on improving our powers. We are responsible for creating the Ice Blade used by the Clan of Angels. The only weapon our clan needs is the Ring of Ice. Using this ring, allows us to create anything ice-related."
+	},
+	{
+		id: "4",
+		name: "Crystal",
+		primaryName: "Clan of Crystals",
+		leader: "Prism",
+		description: "The name gives away why it's a popular clan – The clan found what we call precious: the Spinel Crystal. They are the local specialists of anything to do with the magenta gems. Utilizing those crystals, the Sword of Petrification can turn someone into said crystals. They don't focus much on fight training, their focus is mostly on crystals, as a result, the clan members are sturdy at heart and not in muscle.",
+		onboarding: "I have been waiting for you to arrive, {user.tag}! The Crystal Clan is glad to have you. I am Prism, the clan leader.\n\nOur name gives away why it's a popular clan – our clan found what we call precious: the Spinel Crystal. We are the local specialists of anything to do with the magenta gems. Utilizing those crystals, the Sword of Petrification can turn someone into said crystals. We don't focus on fighting training, our focus is mostly on crystals, as a result, our members are sturdy at heart and not in muscle."
+	},
+	{
+		id: "5",
+		name: "Darkness",
+		primaryName: "Clan of Darkness",
+		leader: "Dolion",
+		description: "The Clan of Darkness is known for being the rudest clan. They spend their days attempting to start fights with other clans however the Clan of Light keeps them in line. One cut from the Dagger of Darkness will turn your body into dark goo. Members of this clan are known for their evil personality.",
+		onboarding: "Are you the new recruit we were supposed to get? – I'm Dolion, leader of the clan. Take this dagger, you might need it later.\n\nOur clan is known for being the rudest clan among them all. We spend our days attempting to start fights with the others, but that Clan of Light keeps stopping us. One cut from the Dagger of Darkness will return your body to dark goo. Our members are known for their evil personality."
+	},
+	{
+		id: "6",
+		name: "Demon",
+		primaryName: "Clan of Demons",
+		leader: "Nyx",
+		description: "One of the oldest clans among them all is the Demon Clan. Known for their violence and being the most ruthless clan there. Creating fireballs through sheer will, they will destroy their enemies without leaving the shadows. Cross paths with a member of the clan means carving your eternal doom into destiny. The Demon Clan overlooks any obstacle and victims a clan member might have.",
+		onboarding: "Ah... You must be the new rookie. {user.tag} is it? Welcome to the clan! I am clan leader Nyx. Now, shall we get you started?\n\nWe, the Demon Clan, are the oldest clans amongst them all. We are known for our violence and being the most ruthless clan there is. Wearing the Necklace of Flames will allow you to create fireballs through sheer. We destroy our enemies without even leaving the shadows. Cross paths with one us means carving your eternal doom into destiny. We overlook any obstacle and any victim our members might have."
+	},
+	{
+		id: "7",
+		name: "Dendro",
+		primaryName: "Clan of Dendros",
+		leader: "Lucian",
+		description: "The Clan of Dendros can create any type of plant using the Gloves of Flora. They can create unbreakable vines to trap their enemies with. Clan members are always with each other and helping each other learn. Starting a fight with one is starting a fight with the whole clan.",
+		onboarding: "Hello, I'm Lucian. Leader of Clan Dendros. You must be {user.tag}. I'll give you a quick summary of our clan.\n\nOur clan can create any type of plant using the Gloves of Flora. We can create unbreakable vines to trap our enemies with. Our members are always with each other and helping each other learn. Starting a fight with us is starting a fight with the whole clan."
+	},
+	{
+		id: "8",
+		name: "Electro",
+		primaryName: "Clan of Electros",
+		leader: "Sparky",
+		description: "The Clan of Electros can create superbolts and energy that can power the whole region. Using the Necklace of Bolts, they can create an energy aura around themselves or if needed, create superbolts that can destroy a whole building. It's always recommended to never start a fight with a clan member.",
+		onboarding: "Hey! I'm Sparky, leader of the Electro Clan! I'm so happy to have you in our clan! Let's get started with your onboarding {user.tag}!\n\nOur electros can create superbolts and energy that can power the whole region! Using the Necklace of Bolts, we can create an energy aura around ourselves or if needed, create the superbolts that can destroy a whole building! It's always recommended that others never start a fight with us."
+	},
+	{
+		id: "9",
+		name: "Geo",
+		primaryName: "Clan of Geos",
+		leader: "Cephas",
+		description: "The Clan of Geos can create earthquakes and forge stones. Using the Ring of Geo, they can create an armor of rocks around themselves, a mud wall, and trigger earthquakes. Clan members are always fighting each other to see who is the strongest among them.",
+		onboarding: "Hello there! I am Cephas, the proud leader of the clan. You must be, {user.tag}. Welcome to our clan! Let me get you up to date.\n\nClan members can create earthquakes and forge stones using the Ring of Geo. We can also create armor of rocks around ourselves and a mud wall to protect ourselves. Our clan members are always fighting each other to see who is the strongest among us."
+	},
+	{
+		id: "10",
+		name: "Ghost",
+		primaryName: "Clan of Ghosts",
+		leader: "Axar",
+		description: "The Clan of the Ghosts don't need doors; they prefer to go through the walls. They materialize wherever and whenever they want, using their sword to vanquish enemies out of this realm. The Dao Spectral Sword can possess an opponent's body. Or, if prefer, turn someone into a ghost. The clan members work hard to keep their reputation and might over-worldly, strengthening their bond in the process.",
+		onboarding: "Boo! Did I scare you? – Haha I did! Welcome to the Ghost Clan's recruit. I'm Axar, the leader of this clan!\n\nWe are the clan that doesn't need doors; we prefer to go through walls. We materialize wherever and whenever we want, using our sword to vanquish enemies out of this realm. Our Dao Spectral Sword can possess an opponent's body. Or, if we prefer it, turn someone into a ghost! Our clan members work hard to keep our reputation and might over-worldly, strengthening our bond in the process!"
+	},
+	{
+		id: "11",
+		name: "Hydro",
+		primaryName: "Clan of Hydros",
+		leader: "coral",
+		description: "The Clan of Hydros can control the water and summon a tsunami. With the Bracelet of Aqua, they can control water wherever it can be found. Clan members are always meditating near a river and training to control the water.",
+		onboarding: "Hello, {user.tag}. I am Coral, leader of Clan Hydros. Come meditate with me while I give you our clan's information.\n\nUs Hydros control the waters and can summon a tsunami. With the Bracelet of Aqua, we can control water wherever it can be found, including from the plants. Our clan members are always meditating near a river and training to control the water."
+	},
+	{
+		id: "12",
+		name: "Jester",
+		primaryName: "Clan of Jesters",
+		leader: "Arlo",
+		description: "The Jester Clan takes advantage of their knowledge of the human psyche. Their magic might not be able to get into minds, but their \"eerie\" behavior is plenty enough. Their magic is stored in ropes that are wrapped around them. These ropes can bring one's fears to life. Or if needed, turn into a sword. Due to nobody being able to withstand being around them, the jesters are a very close-knit clan.",
+		onboarding: "Oh? Are you here to challenge me? – Only a fool would be willing to challenge me! This must mean one thing... you must be the new recruit! Let me introduce myself! I am Arlo, leader of the Jesters. Welcome to the clan... {user.tag}!\n\nHere's a quick summary of us! The Jester Clan, or as the others call us, the \"Deranged Ones\", takes advantage of our members' knowledge of the human psyche. – No, our magic might not be able to get into minds, but that is not needed! Our \"eerie\" behavior is plenty enough. Our magic is stored in ropes that we wrap around ourselves. Those ropes can bring one's fears to life. Or, if needed, turn into a sword. Due to nobody being able to withstand being around us... we are a very close-knit clan!"
+	},
+	{
+		id: "13",
+		name: "Light",
+		primaryName: "Clan of Light",
+		leader: "Chiara",
+		description: "The Clan of Light is known for its bright souls. They focus on keeping the Clan of Darkness in line. Using the Dagger of Light, they can cut through the strongest material. Members of this clan are known for their friendly personality.",
+		onboarding: "Hi {user.tag}, I am Chiara, the leader of the clan. Please take the dagger and follow me. Let me introduce you to the clan.\n\nThe Clan of Light has the brightest souls among them all. We focus on keeping the Clan of Darkness in line as they tend to start fights against other clans. When using the Dagger of Light, we can cut through the strongest material there is. Our members are known for their friendly personality."
+	},
+	{
+		id: "14",
+		name: "Pyro",
+		primaryName: "Clan of Pyros",
+		leader: "Ember",
+		description: "The Clan of Pyros can be found in the volcanic volcano near our region. They control the fire element and erupt the volcano they live in using the Belt of Inferno. They are responsible for creating the Necklace of Flames used by the Clan of Demons. The members of this clan are very self-centered.",
+		onboarding: "Oh hey! I didn't see you there, {user.tag}. Sorry that it's very hot here! Now, let me introduce myself! I am Ember, the leader of the clan.\n\nOur clan can be found in the volcanic volcano near our region. We can control the fire element and erupt the volcano where we live using the Belt of Inferno. We are responsible for the creation of the Necklace of Flames used by the Clan of Demons. Our members are very self-centered."
+	},
+	{
+		id: "15",
+		name: "Witch",
+		primaryName: "Clan of Witches",
+		leader: "Raven",
+		description: "The coven belonging to the Clan of the Witches is found in the forest of death. The witches spend their days casting spells using the Book of Spells. If you ever see a witch, you should run in the other direction. Witches are known for not being friendly and considered dangerous.",
+		onboarding: "Ah, You must be our new witch! I am Raven, queen of the coven and leader of the witches. Take this spell book, let's get you started!\n\nOur coven is located in the forest of death. We spend our days casting and learning spells using our Book of Spells. We are known for not being friendly and are considered dangerous by others. Others usually run in the other direction when they see us."
+	}
+];
